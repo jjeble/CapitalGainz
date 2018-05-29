91df6f21cb7483a3440023161789304f06f89c22
jjeble <jayj2019@gmail.com>
2018-05-29 13:32:48 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
-from app.forms import PostForm
+from app.forms import PostForm, ReusableForm, QuestionsForm
 from guess_language import guess_language
 from flask import jsonify
-
-
+from flask import request
+from flask import flash
 @app.route('/',methods = ['GET','POST'])
 @app.route('/index',methods = ['GET','POST'])
 def index():
@@ -30,6 +30,38 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
+@app.route("/hello", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+    print(form.errors)
+    if request.method == 'POST':
+
+        age=request.form['age']
+
+
+
+        if form.validate():
+            # Save the comment here.
+            flash('ok ' + age)
+            years_for_college = 18 - int(age)
+            info = "That means your child has "+ str(years_for_college) + " years left for college!"
+            flash
+        else:
+            flash("Fill out your child's age )
+
+    return render_template('hello.html', form=form)
+
+@app.route('/questionnaire',methods=['GET','POST'])
+def questions():
+    form = QuestionsForm(request.form)
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        language = request.form.get('age')
+        return '''<h1>The language value is: bal</h1>
+                  <h1>The framework value is: bah</h1>'''
+    return render_template('questions.html',form = form, title='Questions')
+
+
 @app.route('/translate',methods=['POST'])
 def translate_text():
     return jsonify({'text': translate(request.form['text'],
@@ -38,4 +70,9 @@ def translate_text():
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html',title = 'Welcome-Capital Group')
+    return render_template('welcome.html',title = 'Welcome')
+
+@app.route('/options')
+def options():
+    print('hi')
+    return render_template('options.html',title = 'Options')
